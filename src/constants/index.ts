163887e75753db255a7f41221154a86aefce84ec
kokoball <kbp4154@naver.com>
2022-02-26 14:22:54 +0900

    :bug:  Fix: 404 Error 수정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
-export const API_ENDPOINT = '/homeworks/links';
+export const API_ENDPOINT =
+  'https://cors-file-anywhere.herokuapp.com/https://storage-fe.fastraffic.io/homeworks/links';
 export const HOURTIME = 1000 * 60 * 60;
 export const DAYTIME = HOURTIME * 24;
 export const DUMYNOWDATE = 1645782265000;
